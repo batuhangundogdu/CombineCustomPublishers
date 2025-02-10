49dd1e04576f64283e411771b985718a0ec57db3
Batuhan Gündoğdu <gundogdu.batuhan61@gmail.com>
2025-02-10 13:09:56 +0300

    Initial Commit

---
+++ CombineCustomPublishers/CombineCustomPublishersApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineCustomPublishersApp.swift
+//  CombineCustomPublishers
+//
+//  Created by Batuhan GÜNDOĞDU on 10.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct CombineCustomPublishersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
